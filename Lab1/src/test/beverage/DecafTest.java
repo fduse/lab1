f890e35fc2a092bf062a66ae3b617c676ab339c2
dushurenbale <304073067@qq.com>
2016-03-19 21:56:29 +0800

    Decaf ver 2. add Espresso.

--- Lab1/src/test/beverage/DecafTest.java
+++ Lab1/src/test/beverage/DecafTest.java
@@ -8,20 +8,34 @@ import beverage.BeveSize;
 import beverage.Decaf;
 
 public class DecafTest {
-	Decaf d;
+	Decaf ds,dm,dl,dg;
 	@Before
 	public void setUp() throws Exception {
-		d=new Decaf();
-		d.setSize(BeveSize.SMALL);
+		ds=new Decaf();
+		ds.setSize(BeveSize.SMALL);
+		dm=new Decaf();
+		dm.setSize(BeveSize.MEDIUM);
+		dl=new Decaf();
+		dl.setSize(BeveSize.LARGE);
+		dg=new Decaf();
+		dg.setSize(BeveSize.GRANT);
 	}
-	@Test
+	@Test(timeout=1000)
 	public void testCost() {
-		Assert.assertEquals(0.9, d.cost(), 0);//0.4+0.5=0.9
+		Assert.assertEquals(0.9, ds.cost(), 0);//0.4+0.5=0.9
+		Assert.assertEquals(1.2, dm.cost(), 0);//0.7+0.5=1.2
+		Assert.assertEquals(1.5, dl.cost(), 0);//1.0+0.5=1.5
+		Assert.assertEquals(1.8, dg.cost(), 0);//1.3+0.5=1.8
+
+		
 	}
 
-	@Test
+	@Test(timeout=1000)
 	public void testGetDescription() {
-		
+		Assert.assertEquals("Decaf", ds.getDescription());
+		Assert.assertEquals("Decaf", dm.getDescription());
+		Assert.assertEquals("Decaf", dl.getDescription());
+		Assert.assertEquals("Decaf", dg.getDescription());
 	}
 
 }
---
+++ Lab1/src/test/beverage/EspressoTest.java
@@ -0,0 +1,43 @@
+package test.beverage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import beverage.BeveSize;
+import beverage.Espresso;
+
+public class EspressoTest {
+	Espresso es,em,el,eg;
+	
+	@Before
+	public void setUp() throws Exception {
+		es=new Espresso();
+		es.setSize(BeveSize.SMALL);
+		em=new Espresso();
+		em.setSize(BeveSize.MEDIUM);
+		el=new Espresso();
+		el.setSize(BeveSize.LARGE);
+		eg=new Espresso();
+		eg.setSize(BeveSize.GRANT);
+	}
+	@Test(timeout=1000)
+	public void testCost() {
+		Assert.assertEquals(1.4, es.cost(), 0);//0.4+1.0=0.4
+		Assert.assertEquals(1.7, em.cost(), 0);//0.7+1.0=1.7
+		Assert.assertEquals(2.0, el.cost(), 0);//1.0+1.0=2.0
+		Assert.assertEquals(2.3, eg.cost(), 0);//1.3+1.0=2.3
+
+		
+	}
+
+	@Test(timeout=1000)
+	public void testGetDescription() {
+		Assert.assertEquals("Esspresso", es.getDescription());
+		Assert.assertEquals("Esspresso", em.getDescription());
+		Assert.assertEquals("Esspresso", el.getDescription());
+		Assert.assertEquals("Esspresso", eg.getDescription());
+	}
+		
+
+}

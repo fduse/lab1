e9cade9b5558a8276151dbb975bc12777d780f04
dushurenbale <304073067@qq.com>
2016-03-21 23:33:22 +0800

    Main test of dealanorder and dealorders.

--- Lab1/src/lab1/Main.java
+++ Lab1/src/lab1/Main.java
@@ -7,128 +7,245 @@ import ingredient.Milk;
 import ingredient.WhipCream;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 import beverage.BeveSize;
 import beverage.Beverage;
+import beverage.Decaf;
 import beverage.Espresso;
 import beverage.GreenTea;
 import beverage.HouseBlend;
 import beverage.RedTea;
 import beverage.WhiteTea;
 
+
+
 public class Main {
-	
-	public static void main(String[] args) {	
-		String[] disArr = new String[args.length];
-		for (int j = 0; j < args.length; j++) {
-			disArr[j] = args[j].toLowerCase();
-		}
-		
-		dealAnOrder(disArr);
-	}
 
-	private static void dealAnOrder(String[] disArr) {
-		int i;  // beverage name's length(may be 1 or 2)
-		BeveSize size = BeveSize.EMPTY;  // beverage's size
-		for (i = 0; i < disArr.length; i++) {
-			if (disArr[i].matches("^small$|^medium$|^large$|^grant$")) {
-				size = BeveSize.valueOf(disArr[i].toUpperCase());
-				break;
-			}
-		}
+    public static void main(String[] args) {
 
-		if (i >= disArr.length) {
-			showIllegalInput("Must set a size!");
-			return;
-		}
-
-		// get beverage's name
-		String beveStr = disArr[0] + ((i == 2) ? " " + disArr[1] : "");  
-		
-		// create base beverage
-		Beverage order;
-		if (beveStr.equals("espresso")) {
-			order = new Espresso();
-			order.setSize(size);
-		} else if (beveStr.equals("houseblend")) {
-			order = new HouseBlend();
-			order.setSize(size);
-		} else if (beveStr.equals("mocha")) {
-			order = new Espresso();
-			order.setSize(size);
-			order = new Chocolate(order);
-		} else if (beveStr.equals("latte")) {
-			order = new Espresso();
-			order.setSize(size);
-			order = new Milk(order);
-		} else if (beveStr.equals("cappuccino")) {
-			order = new Espresso();
-			order.setSize(size);
-			order = new WhipCream(order);
-		} else if (beveStr.equals("green tea")) {
-			order = new GreenTea();
-			order.setSize(size);
-		} else if (beveStr.equals("red tea")) {
-			order = new RedTea();
-			order.setSize(size);
-		} else if (beveStr.equals("white tea")) {
-			order = new WhiteTea();
-			order.setSize(size);
-		} else if (beveStr.equals("flower tea")) {
-			order = new GreenTea();
-			order.setSize(size);
-			order = new Jasmine(order);
-		} else if (beveStr.equals("ginger tea")) {
-			order = new GreenTea();
-			order.setSize(size);
-			order = new Ginger(order);
-		} else if (beveStr.equals("tea latte")) {
-			order = new RedTea();
-			order.setSize(size);
-			order = new Milk(order);
-		} else {
-			showIllegalInput(beveStr);
-			return;
-		}
+        if (args.length == 0){
+            System.err.println("No order provided.");
+            return;
+        }
 
 
-		// add ingredient
-		for (i++; i < disArr.length; i++) {
-			if (disArr[i].equals("chocolate")) {
-				order = new Chocolate(order);
-			} else if (disArr[i].equals("ginger")) {
-				order = new Ginger(order);
-			} else if (disArr[i].equals("milk")) {
-				order = new Milk(order);
-			} else if (disArr[i].equals("jasmine")) {
-				order = new Jasmine(order);
-			} else if (disArr[i].equals("whip")) {
-				i++;  // 2 words "whip cream"
-				order = new WhipCream(order);
-			} else {
-				showIllegalInput(disArr[i]);
-			}
-		}
+        String[] disArr = new String[args.length];
+        for (int j = 0; j < args.length; j++) {
+            disArr[j] = args[j].toLowerCase();
+//            System.out.println(args[j]);
+        }
 
-		/**
-		 * How do I get the description of each order instead of doing this
-		 * stupid thing forever (except for printing the args)?
-		 */
-		/*if (order instanceof BeverageWithIngredient) {
-			((BeverageWithIngredient) order).getDescription();
-		} else if (order instanceof Espresso) {
-			((Espresso) order).getDescription();
-		}*/
-		// and so on...
 
-		System.out.println("The order is: " + order.getDescription());
-		
-		DecimalFormat df = new DecimalFormat(".0");
-		System.out.println("The total cost of your order is: "
-				+ df.format(order.cost()));
-	}
-	
-	private static void showIllegalInput(String errMsg) {
-		System.err.println("Illegal input:" + errMsg);
-	}
+        if(isNum(disArr[0])) {
+            //第一个arg是number,说明是多个orders
+            try{
+            double cost = dealOrders(disArr);
+            displayCost(cost);
+            }catch (Exception e){
+                e.printStackTrace();
+                return;
+            }
+
+
+        }else{
+            try{
+                double cost = dealAnOrder(disArr);
+                displayCost(cost);
+            } catch (Exception e){
+                e.printStackTrace();
+                return;
+            }
+
+        }
+    }
+    private static boolean isNum(String str) {
+        return str.matches("[0-9]+");
+    }
+   //单元测试时改成Public
+    public static double dealOrders(String[] disArr) throws Exception{
+        String str = "";
+
+
+        //得到订单数
+        int count = Integer.parseInt(disArr[0]);
+
+        //escape the first element: order number
+        //把所有args连起来再通过;分割
+        for (int i = 1; i < disArr.length; i++){
+            if(i != disArr.length-1 && !disArr[i].equals(";")){
+            str += (disArr[i]+" ");
+            }else{
+                str += disArr[i];
+            }
+        }
+        //分割
+        String[] splittedArr = str.split(";");
+
+
+        if (splittedArr.length != count){
+            String errMsg = "wrong input for ';'";
+            showIllegalInput(errMsg);
+            throw new Exception(errMsg);
+        }
+
+
+        ArrayList<String[]> disArrsList = new ArrayList<>();
+
+        for (int i = 0; i < splittedArr.length; i++){
+            String[] tempDisArr = splittedArr[i].split(" ");
+            disArrsList.add(tempDisArr);
+        }
+
+        String[][] disArrs =  (String[][])disArrsList.toArray(new String[disArrsList.size()][]);
+
+        double totalCost = 0;
+
+        for(int i = 0; i < disArrs.length; i++){
+            double cost =  dealAnOrder(disArrs[i]);
+            totalCost += cost;
+        }
+
+            return totalCost;
+
+    }
+
+
+
+    //单元测试时改成Public,平时是Private
+
+    public static double dealAnOrder(String[] disArr) throws Exception{
+        int i;  // beverage name's length(may be 1 or 2)
+        BeveSize size = BeveSize.EMPTY;  // beverage's size
+        for (i = 0; i < disArr.length; i++) {
+            if (disArr[i].matches("^small$|^medium$|^large$|^grant$")) {
+                size = BeveSize.valueOf(disArr[i].toUpperCase());
+                break;
+            }
+        }
+
+        if (i >= disArr.length) {
+            showIllegalInput("Must set a size!");
+            throw new Exception();
+        }
+
+        // get beverage's name
+        String beveStr = disArr[0] + ((i == 2) ? " " + disArr[1] : "");
+
+        // create base beverage
+        Beverage order;
+        switch (beveStr) {
+        	case "decaf":
+        		order = new Decaf();
+        		order.setSize(size);
+        		break;
+        	case "decaf mocha":
+        		order = new Decaf();
+        		order.setSize(size);
+        		order = new Chocolate(order);
+        		break;
+        	case "decaf latte":
+        		order = new Decaf();
+        		order.setSize(size);
+        		order = new Milk(order);
+        		break;
+        	case "decaf cappuccino":
+        		order = new Decaf();
+        		order.setSize(size);
+        		order = new WhipCream(order);
+        		break;
+            case "espresso":
+                order = new Espresso();
+                order.setSize(size);
+                break;
+            case "houseblend":
+                order = new HouseBlend();
+                order.setSize(size);
+                break;
+            case "mocha":
+                order = new Espresso();
+                order.setSize(size);
+                order = new Chocolate(order);
+                break;
+            case "latte":
+                order = new Espresso();
+                order.setSize(size);
+                order = new Milk(order);
+                break;
+            case "cappuccino":
+                order = new Espresso();
+                order.setSize(size);
+                order = new WhipCream(order);
+                break;
+            case "green tea":
+                order = new GreenTea();
+                order.setSize(size);
+                break;
+            case "red tea":
+                order = new RedTea();
+                order.setSize(size);
+                break;
+            case "white tea":
+                order = new WhiteTea();
+                order.setSize(size);
+                break;
+            case "flower tea":
+                order = new GreenTea();
+                order.setSize(size);
+                order = new Jasmine(order);
+                break;
+            case "ginger tea":
+                order = new GreenTea();
+                order.setSize(size);
+                order = new Ginger(order);
+                break;
+            case "tea latte":
+                order = new RedTea();
+                order.setSize(size);
+                order = new Milk(order);
+                break;
+            default:
+                showIllegalInput(beveStr);
+                throw new Exception();
+        }
+
+
+
+        // add ingredient
+        for (i++; i < disArr.length; i++) {
+            switch (disArr[i]) {
+                case "chocolate":
+                    order = new Chocolate(order);
+                    break;
+                case "ginger":
+                    order = new Ginger(order);
+                    break;
+                case "milk":
+                    order = new Milk(order);
+                    break;
+                case "jasmine":
+                    order = new Jasmine(order);
+                    break;
+                case "whip":
+                    i++;
+                    order = new WhipCream(order);
+                    break;
+                default:
+                    showIllegalInput(disArr[i]);
+                    throw new Exception();
+            }
+        }
+
+        return order.cost();
+    }
+
+    private static void displayCost(double cost){
+        DecimalFormat df = new DecimalFormat(".0");
+        System.out.println("The total cost of your order is: "
+                + df.format(cost));
+    }
+
+    private static void showIllegalInput(String errMsg) {
+        System.err.println("Illegal input:" + errMsg);
+    }
 }

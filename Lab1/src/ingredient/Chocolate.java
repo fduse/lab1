0dab9f460f952fdc71dc4d051c1e39091833ada7
wjt <wjt128@gmail.com>
2016-03-21 12:45:00 +0800

    完成新功能

--- Lab1/src/ingredient/Chocolate.java
+++ Lab1/src/ingredient/Chocolate.java
@@ -3,15 +3,17 @@ package ingredient;
 import beverage.Beverage;
 
 public class Chocolate extends BeverageWithIngredient {
-	
+
 	public Chocolate(Beverage drink) {
 		super(drink);
 	}
 
+
+
 	public String getDescription() {
 		return super.getDescription() + " chocolate";
 	}
-	
+
 	public double cost() {
 		return 0.3 + super.cost();
 	}
